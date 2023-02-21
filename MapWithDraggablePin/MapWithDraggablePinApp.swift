43acd949ee2457063f5dcaaed2e5b59a5f61d2bf
MiniMouce <minimooooose@gmail.com>
2023-02-21 17:48:16 +0300

    Initial Commit

---
+++ MapWithDraggablePin/MapWithDraggablePinApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapWithDraggablePinApp.swift
+//  MapWithDraggablePin
+//
+//  Created by Инга on 21.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct MapWithDraggablePinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
