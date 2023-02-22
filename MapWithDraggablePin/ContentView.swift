4f16673ee4fe0e605f6e37d2eb9d786066a630bc
MiniMouce <minimooooose@gmail.com>
2023-02-22 09:35:23 +0300

    Commit 1

--- MapWithDraggablePin/ContentView.swift
+++ MapWithDraggablePin/ContentView.swift
@@ -6,16 +6,111 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
+    
+    @State var title = ""
+    @State var subtitle = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        ZStack(alignment: .bottom, content: {
+            MapView(title: self.$title, subtitle: self.$subtitle).edgesIgnoringSafeArea(.all)
+            
+            if self.title != ""{
+                HStack(spacing: 12){
+                    Image(systemName: "info.circle.fill").font(.largeTitle).foregroundColor(.black)
+                    
+                    VStack(alignment: .leading, spacing: 15){
+                        Text(self.title).font(.body).foregroundColor(.black)
+                        Text(self.subtitle).font(.caption).foregroundColor(.gray)
+                        
+                    }
+                }.padding()
+                .background(Color("Color"))
+                .cornerRadius(15)
+            }
+        })
+            
+            
+        }
     }
-}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
+struct MapView : UIViewRepresentable {
+    
+    func makeCoordinator() -> MapView.Coordinator {
+        
+        return MapView.Coordinator(parent1: self)
+    }
+    
+    @Binding var title : String
+    @Binding var subtitle : String
+    
+    func makeUIView(context: UIViewRepresentableContext<MapView>) -> MKMapView{
+        
+        let map = MKMapView()
+        
+        let coordinate = CLLocationCoordinate2D(latitude: 55.751244, longitude: 37.618423)
+        
+        map.region = MKCoordinateRegion(center: coordinate, latitudinalMeters: 10000, longitudinalMeters: 10000)
+        
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = coordinate
+        
+        map.delegate = context.coordinator
+        
+        map.addAnnotation(annotation)
+        
+        return map
+    }
+    
+    func updateUIView(_ uiView: MKMapView, context: UIViewRepresentableContext<MapView>) {
+        
+    }
+    
+    class Coordinator : NSObject,MKMapViewDelegate{
+        
+        var parent : MapView
+        
+        init(parent1: MapView) {
+            
+            parent = parent1
+        }
+        
+        func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+            
+            let pin = MKPinAnnotationView(annotation: annotation, reuseIdentifier: "pin")
+            pin.isDraggable = true
+            pin.pinTintColor = .red
+            pin.animatesDrop = true
+            
+            return pin
+    }
+
+        func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, didChange newState: MKAnnotationView.DragState, fromOldState oldState: MKAnnotationView.DragState) {
+            
+            CLGeocoder().reverseGeocodeLocation(CLLocation(
+             latitude:(view.annotation?.coordinate.latitude)!,
+             longitude:(view.annotation?.coordinate.longitude)!)) {
+            (places, err) in
+                
+                if err != nil{
+                    print((err?.localizedDescription)!)
+                    return
+                }
+                self.parent.title = (places?.first?.name ?? places?.first?.postalCode)!
+                self.parent.subtitle = (places?.first?.locality ?? places?.first?.country ?? "None")
+            }
+            }
+        }
+
+}
+
